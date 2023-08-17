6a14a6efd96434f4b57488d21f1af0bb0f8b46f4
Manoj Kumar <kmanoj@iihs.ac.in>
2023-08-17 14:44:12 +0530

    more functionality added

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,8 +1,21 @@
 $(document).ready(() => {
+    console.log('im here')
     const nameElm = $('#name');
     const timeElm = $('#time');
     const formElm = $('form');
+    //create a new alarm
+const currentDate = new Date();
+const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+const currentDay = currentDate.getDate().toString().padStart(2, '0');
+//same as before, add 0 to month and day if they're less than 10
+browser.alarms.create(alarmName, {
+  when: new Date(
+    currentDate.getFullYear() + '-' + currentMonth + '-' + currentDay + 'T' + timeElm.val()
+  ).getTime(),
+  periodInMinutes: 1440,
+});
     formElm.on('submit', () => {
+        console.log('hi')
         $('.alert').remove(); //remove previous success alerts, if any
         const self = this;
         //get existing alarms
